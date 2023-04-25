9c189d79dffeb0b7dc1adbbbda5f484e077542c4
leifhauskendev <leifhauskendev@gmail.com>
2023-04-24 21:18:46 -0500

    Added initial commits for AESHelper and PasswordInfo classes

---
+++ PasswordCatacomb/AESHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class AesHelper
+{
+    private static readonly byte[] salt = Encoding.UTF8.GetBytes("This is my salt!");
+
+    public static byte[] EncryptStringToBytes(string plainText, byte[] key, byte[] iv)
+    {
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (key == null || key.Length <= 0)
+            throw new ArgumentNullException("key");
+        if (iv == null || iv.Length <= 0)
+            throw new ArgumentNullException("iv");
+        byte[] encrypted;
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.IV = iv;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.CBC;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(plainText);
+                    }
+                    encrypted = ms.ToArray();
+                }
+            }
+        }
+        return encrypted;
+    }
+
+    public static string DecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
+    {
+        if (cipherText == null || cipherText.Length <= 0)
+            throw new ArgumentNullException("cipherText");
+        if (key == null || key.Length <= 0)
+            throw new ArgumentNullException("key");
+        if (iv == null || iv.Length <= 0)
+            throw new ArgumentNullException("iv");
+        string decrypted;
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.IV = iv;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.CBC;
+
+            using (MemoryStream ms = new MemoryStream(cipherText))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cs))
+                    {
+                        decrypted = sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+        return decrypted;
+    }
+
+    public static byte[] GenerateKey(string password)
+    {
+        using (var rfc = new Rfc2898DeriveBytes(password, salt))
+        {
+            return rfc.GetBytes(32); // 32 bytes = 256 bits
+        }
+    }
+
+    public static byte[] GenerateIV()
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.GenerateIV();
+            return aes.IV;
+        }
+    }
+}
+
